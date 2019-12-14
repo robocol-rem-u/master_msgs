9e515ce98f827b932f01473fccadb8e695402aff
FranzLuepke <kevin.luepke@gmail.com>
2019-12-14 09:37:16 -0500

    arm_auto_status msg created.

--- node_Interface.py
+++ node_Interface.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 import rospy
-from master_msgs.msg import traction_Orders, imu_Speed, imu_Magnetism, pots, current, rpm, arm_Orders, goal, connection
+from master_msgs.msg import traction_Orders, imu_Speed, imu_Magnetism, pots, current, rpm, arm_Orders, goal,connection
 
 def node_Interface():
     rospy.init_node('node_Interface',anonymous=True)
-    rospy.Subscriber('topic_Traction_Orders', traction_Orders, traction_Orders_Callback)
-    rospy.Subscriber('topic_IMU_Speed', imu_Speed, IMU_Speed_Callback)
-    rospy.Subscriber('topic_IMU_Magnetism', imu_Magnetism, IMU_Magnetism_Callback)
-    rospy.Subscriber('topic_Pots', pots, pots_Callback)
-    rospy.Subscriber('topic_Current', current, current_Callback)
-    rospy.Subscriber('topic_RPM', RPM, rpm_Callback)
-    rospy.Subscriber('topic_Arm_Orders', arm_Orders, arm_Orders_Callback)
-    pub_Goal = rospy.Publisher('topic_Goal', goal, queue_size=10)
+    rospy.Subscriber('topic_Traction_Orders',traction_Orders,traction_Orders_Callback)
+    rospy.Subscriber('topic_IMU_Speed',imu_Speed,IMU_Speed_Callback)
+    rospy.Subscriber('topic_IMU_Magnetism',imu_Magnetism,IMU_Magnetism_Callback)
+    rospy.Subscriber('topic_Pots', pots,pots_Callback)
+    rospy.Subscriber('topic_Current',current,current_Callback)
+    rospy.Subscriber('topic_RPM', RPM,rpm_Callback)
+    rospy.Subscriber('topic_Arm_Orders',arm_Orders,arm_Orders_Callback)
+    pub_Goal = rospy.Publisher('topic_Goal',goal, queue_size=10)
     pub_Arm_Orders = rospy.Publisher('topic_Arm_Orders', arm_Orders, queue_size=10)
     pub_Connection = rospy.Publisher('topic_Connection', connection, queue_size=10)
     pub_Traction_Orders = rospy.Publisher('topic_Traction_Orders', traction_Orders, queue_size=10)
